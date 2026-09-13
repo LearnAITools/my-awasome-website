3e81c4a6b09904c80f757377c269bd665e6eb103
Shashi Kumar <sk00009387@gmail.com>
2026-09-13 18:40:36 +0530

    face changes

---
+++ frontend/lib/types/auth/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Authentication service type exports
+ */
+export type { LoginRequest, SignupRequest, UpdateProfileRequest, EmailChangeRequest, EmailChangeVerifyRequest } from './requests'
+export type { AuthResponse, UserProfileResponse, UserSummary, UserIdentitySummary, EmailChangeOtpResponse } from './responses'
---
+++ frontend/lib/types/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Central type exports for all API contracts
+ * Organized by service domain
+ */
+
+// Common types and errors
+export * from './common'
+
+// Auth service types
+export * from './auth'
+
+// Booking service types
+export * from './booking'
+
+// Payment service types
+export * from './payment'
+
+// Movie service types
+export * from './movies'
